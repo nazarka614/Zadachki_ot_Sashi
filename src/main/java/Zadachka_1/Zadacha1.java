146763948f8e92a3ca82abc1bb9a389d2b675421
Nazar.Nerubayko <nazarka614@gmail.com>
2022-10-31 13:13:50 +0200

    - Zadachka 1

--- src/main/java/Zadachka_1/Zadacha1.java
+++ src/main/java/Zadachka_1/Zadacha1.java
@@ -1,4 +1,18 @@
 package Zadachka_1;
 
 public class Zadacha1 {
+    public static void main(String[] args) {
+        System.out.println(stringReverse("Nazar"));
+    }
+
+
+    public static String stringReverse(String string){
+        char[] charArray = string.toCharArray();
+        String resultString = "";
+        for (int i = charArray.length - 1; i >= 0; i--) {
+            resultString += charArray[i];
+        }
+        return resultString;
+    }
 }
+
