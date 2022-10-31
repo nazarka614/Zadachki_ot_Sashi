4aae54eb0f59a4f87becf4753c91543df4a692e9
Nazar.Nerubayko <nazarka614@gmail.com>
2022-10-31 13:25:31 +0200

    - Zadachka 2

--- src/main/java/Zadachka_2/Zadachka2.java
+++ src/main/java/Zadachka_2/Zadachka2.java
@@ -1,4 +1,20 @@
 package Zadachka_2;
 
 public class Zadachka2 {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("qwewq9"));
+    }
+
+    public static boolean isPalindrome(String string){
+        boolean ispoli = false;
+        char[] charArray = string.toCharArray();
+        String resultString = "";
+        for (int i = charArray.length - 1; i >= 0; i--) {
+            resultString += charArray[i];
+        }
+        if (string.equals(resultString)){
+            ispoli = true;
+        }
+        return ispoli;
+    }
 }
